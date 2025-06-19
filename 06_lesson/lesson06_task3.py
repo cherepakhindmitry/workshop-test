d25d471fbdd583ddf6daeaee9753e11630737944
Chere <cherepakhindmitry@gmail.com>
2025-06-19 20:15:03 +0400

    Доработан тест: ожидание загрузки всех изображений (Done!)

--- 06_lesson/lesson06_task3.py
+++ 06_lesson/lesson06_task3.py
@@ -5,23 +5,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Создаем Chrome-драйвер
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-try:
-    # Переход на сайт с ленивой загрузкой изображений
-    driver.get(
-        "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html"
-    )
+def test_images_loaded():
+    # Создаем Chrome-драйвер
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    wait = WebDriverWait(driver, 15)
 
-    # Ждём, пока загрузится 3-я картинка (по CSS: img:nth-of-type(3))
-    wait = WebDriverWait(driver, 10)
-    third_img = wait.until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "img:nth-of-type(3)"))
-    )
+    try:
+        # Открываем страницу
+        driver.get(
+            "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html"
+        )
 
-    # Получаем атрибут "src" (ссылка на изображение)
-    print(third_img.get_attribute("src"))
+        # Ждем появления текста "Done!"
+        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "p"), "Done!"))
 
-finally:
-    driver.quit()
+        # Получаем третье изображение
+        third_img = driver.find_element(By.CSS_SELECTOR, "img:nth-of-type(3)")
+        src = third_img.get_attribute("src")
+        print(f"Третье изображение загружено: {src}")
+
+        # Проверка
+        assert "img" in src, f"Ожидалась ссылка на изображение, но получили: {src}"
+
+    finally:
+        driver.quit()
